e780f5248fba98d389e022d0f489a32260c8b95f
Ianara-cs <ianarasilva333@gmail.com>
2022-09-26 16:14:45 -0300

    .

--- bmiaApp-svelte/src/components/Form.svelte
+++ bmiaApp-svelte/src/components/Form.svelte
@@ -1,14 +1,4 @@
 <script lang="ts">
-      let name: string, input_weight: string, input_height: string
-
-    function Calcular() {
-        const weight = Number(input_weight)
-        const height = Number(input_height)
-
-        const bmi = weight / (height*height)
-
-        alert('BMI = ' + bmi.toFixed(1))
-    }
 </script>
 
 <form >
